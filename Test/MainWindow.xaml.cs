dd912f40f1ad7c699914bc538990b43126bf36bc
ispeedonthe405 <ispeedonthe405@gmail.com>
2024-07-05 04:56:57 -0700

    Updated Datagrid template

--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,11 +13,32 @@ using WpfThemer;
 
 namespace Test
 {
+    internal class DgridItem
+    {
+        public string Value0 { get; set; } = string.Empty;
+        public string Value1 { get; set; } = string.Empty;
+        public string Value2 { get; set; } = string.Empty;
+
+        public static DgridItem Generate()
+        {
+            Random rnd = new();
+            DgridItem item = new()
+            {
+                Value0 = rnd.NextDouble().ToString(),
+                Value1 = rnd.NextDouble().ToString(),
+                Value2 = rnd.NextDouble().ToString()
+            };
+            return item;
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<DgridItem> GridItems { get; set; } = [];
+
         public MainWindow()
         {
             InitializeComponent();
@@ -24,6 +46,12 @@ namespace Test
             cb_Theme.ItemsSource = ThemeManager.Themes;
             cb_Theme.DisplayMemberPath = "DisplayName";
             cb_Theme.SelectedItem = ThemeManager.ActiveTheme;
+
+            for(int i = 0; i < 10; i++)
+            {
+                GridItems.Add(DgridItem.Generate());
+            }
+            datagrid.ItemsSource = GridItems;
         }
 
         private void cb_Theme_SelectionChanged(object sender, SelectionChangedEventArgs e)
